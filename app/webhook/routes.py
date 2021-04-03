f1ed195df3e83593ba2ae9a913667a638cbad331
harshit-02 <s.harshit0210@gmail.com>
2021-04-03 23:17:47 +0530

    update

--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -5,4 +5,6 @@ webhook = Blueprint('Webhook', __name__, url_prefix='/webhook')
 @webhook.route('/receiver', methods=["POST"])
 def receiver():
     if request.headers['Content-Type'] == 'application/json':
+        info=json.dumps(request.json)
+        print(info)
         return {'harshit':'webhook'}, 200
